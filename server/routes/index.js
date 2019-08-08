5d7d86e3ffb60f55eb03fdcb722caf2f5906f422
ardynn14 <ardynn.d@gmail.com>
2019-08-08 21:39:14 +0700

    add fitur bmi

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 const userRoutes = require('./userRoutes')
+const bmiRoutes = require('./bmiRoutes')
 
 router.use('/users', userRoutes)
+router.use('/getBmi', bmiRoutes)
 
 module.exports = router
