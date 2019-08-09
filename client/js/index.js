7121f632c78ba101b4db04c5fe11b7f29add86da
ardynn14 <ardynn.d@gmail.com>
2019-08-09 10:45:40 +0700

    formlogin

--- client/js/index.js
+++ client/js/index.js
@@ -1,12 +1,32 @@
 const baseUrl = `http://localhost:3000`
 
-$(document).ready(function(){
+$(document).ready(function () {
   isLogin()
+  renderButton()
 })
 
+function onSuccess(googleUser) {
+  console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+}
+
+function onFailure(error) {
+  console.log(error);
+}
+
+function renderButton() {
+  gapi.signin2.render('my-signin2', {
+    'scope': 'profile email',
+    'width': 230,
+    'height': 40,
+    'longtitle': true,
+    'theme': 'light',
+    'onsuccess': onSuccess,
+    'onfailure': onFailure
+  });
+}
 //============= Hide and Show =============
-function isLogin(){
-  if (localStorage.getItem('token')){
+function isLogin() {
+  if (localStorage.getItem('token')) {
     hasToken()
   }
   else {
@@ -14,13 +34,13 @@ function isLogin(){
   }
 }
 
-function hasToken(){
+function hasToken() {
   $('.mainWeb').show()
   // $('#buttonSignIn').hide()
   // $('#buttonSignOut').show()
 }
 
-function noToken(){
+function noToken() {
   $('.mainWeb').hide()
   // $('#buttonSignIn').show()
   // $('#buttonSignOut').hide()
@@ -28,21 +48,21 @@ function noToken(){
 
 //============= Google Signin =============
 function onSignIn(googleUser) {
-  const idToken= googleUser.getAuthResponse().id_token
+  const idToken = googleUser.getAuthResponse().id_token
   axios({
     url: `${baseUrl}/loginGoogle`,
     method: 'post',
     dataType: 'json',
-    data: {idToken}
+    data: { idToken }
   })
-    .then(({data})=>{
+    .then(({ data }) => {
       localStorage.setItem('token', data.token)
       localStorage.setItem('_id', data._id)
       localStorage.setItem('name', data.name)
       localStorage.setItem('email', data.email)
       hasToken()
     })
-    .catch((err)=>{
+    .catch((err) => {
       console.log(err)
     })
 }
