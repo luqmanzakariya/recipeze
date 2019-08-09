c2c64aa6e759813b82b21a08ceed0a9c13781727
nurfitrys <nurftrys@gmail.com>
2019-08-09 13:08:37 +0700

    commit

--- server/controllers/youtubeController.js
+++ server/controllers/youtubeController.js
@@ -1,22 +1,24 @@
-// const axios = require('axios')
-// const youtube = axios.create({
-//     baseURL : 'https://www.googleapis.com/youtube'
-// })
+const axios = require('axios')
+const youtube = axios.create({
+    baseURL : 'https://www.googleapis.com/youtube'
+})
+const YOUTUBE_TOKEN = process.env.YOUTUBE_TOKEN
 
-// class youtubeController {
-//     static search (req, res, next) {
-//         youtube.get('/v3/search')
-//         .then(function ({ data }) {
-//             if (req.query.search) {
-//                 data = data.filter(newData => RegExp(req.query.search, 'i').test(newData.items))
-//                 // console.log(data)
-//             }
-//             res.status(200).json(data)
-//         })
-//         .catch(next)
-//     }
-// }
+class youtubeController {
+    static search (req, res, next) {
+        youtube.get(`/v3/search${YOUTUBE_TOKEN}${req.query.search} food&part=id`)
+        .then(function ({ data }) {
+            if (req.query.search) {
+                data = data.filter(newData => RegExp(req.query.search, 'i').test(newData))
+            }
+            res.status(200).json(data)
+        })
+        .catch(next)
+    }
+}
 
-// module.exports = youtubeController
+module.exports = youtubeController
 
-// https://www.googleapis.com/youtube/v3/search?key=AIzaSyAwye9ifFpQSoShTuOa_nnbAhXbDsQaXOk&q=jagung&part=id
+// AIzaSyAwye9ifFpQSoShTuOa_nnbAhXbDsQaXOk
+
+// https://www.googleapis.com/youtube/v3/search=?key=AIzaSyACtpKsGX6kCjHm-PpC9gNIjZCRaN5RrMY&q=food&part=id
